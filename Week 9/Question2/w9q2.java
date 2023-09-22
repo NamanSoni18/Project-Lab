c3fa5af59d522c8c848368beee0b9a1a270931f0
Naman Soni <97269749+NamanSoni18@users.noreply.github.com>
2023-09-22 22:21:17 +0530

    Update w9q2.java

--- Week 9/Question2/w9q2.java
+++ Week 9/Question2/w9q2.java
@@ -1,6 +1,4 @@
-/* Create class University, which has data members- name and ranking. Create class Faculty that extends University class has data member- name and method- Details(). Create a new class 
-Department which is derived from Faculty and has data member- name, chairman and method Details() 
-and Display() where Display() method calls Details() methods of both Faculty and Department class in its body. Create an object of Department class to Display() method and University ranking. */
+/* Create class University, which has data members- name and ranking. Create class Faculty that extends University class has data member- name and method- Details(). Create a new class Department which is derived from Faculty and has data member- name, chairman and method Details() and Display() where Display() method calls Details() methods of both Faculty and Department class in its body. Create an object of Department class to Display() method and University ranking. */
 
 package Question2;
 
